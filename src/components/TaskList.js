5c4d3e713a9284ec1d9743e8769cc48370dce41f
NursahDemirpolat <nursahdmrplt@gmail.com>
2023-05-23 12:39:05 +0300

    Listeleme İşlemleri Css Özellikleri

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import TasksShow from "./TaskShow";
 function TaskList({tasks}) {
 
     return (
-        <div>
+        <div className="task-list">
             {tasks.map((task) => {   ///Array fonsiyonunu dönüyoruz map ile task ile bilgiyi alıyoruz
                 return (
                     <TasksShow key={task.id} task={task}/>
--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -2,8 +2,15 @@ function TasksShow({task}) {
     console.log(task)
 
     return ( 
-        <div>
-
+        <div className="tasklar">
+            <h3>Göreviniz</h3>
+            <p>{task.title}</p>
+            <h3>Yapılacaklar</h3>
+            <p>{task.taskDesc}</p>
+            <div>
+                <button className="button-delete">Sil</button>
+                <button className="button-edit">Güncelle</button>
+            </div>
         </div>
      );
 }
